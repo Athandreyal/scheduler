54cc00920588c9c0e28fc4ce2afc144cda490da0
Athandreyal <darkprev@hotmail.com>
2019-02-13 02:17:04 -0700

    why must this be so difficult?
    * scraping is now as complete as I think I can make it
    * fixed a few parsing errors
    * replaced a number of special chars that were getting through into strings
    * evicted the delays, their webserver is rather....tolerant
    * builtin a <short> conversion table for a set of special cases
      which results allows parsing data for 49 more programs than otherwise
    
    
    - parser is one step away from a unified dict of class structs
    - parser does not yet attempt realing with contexual parsing of reqs/antireqs, etc.
      - that might be nasty. It'll have to deal with:
        - context:  'any of','one of', 'not', 'and', 'or' and perhaps others
        - designations: inconsistent prefixing of program, eg Bio 321 or 323
        - program changes: eg, PMAT 318 or CPSC 418, which is for the infosec requisite
    - parser does not yet handle degree requisites.
      - not sure where I find that info in an always publicly available form
    
    Issues:
    	there are 52 programs I cannot find any information on.
    	A couple I was able to find results in google, but was challenged for credentialsI apparently lack...
    
    
    
    Signed-off-by: Athandreyal <darkprev@hotmail.com>

--- scraper.py
+++ scraper.py
@@ -59,6 +59,7 @@ contacts_departments_load_fail = False
 contacts_classes_load_fail = False
 repeat = 0
 
+name_to_short = {}
 
 class CLASS:
     def __init__(self,d,c,s):
@@ -81,7 +82,7 @@ class CLASS:
     
 class DEPT:
     def __init__(self,short=None,name=None,IDS={}):
-        self.name_short=short
+        self.short=short
         self.name=name
         self.IDS={}  #ID's should reference a CLASS object
         
@@ -95,7 +96,6 @@ class FACULTY:
         self.research=research
     
 def save_objs(path,obj):
-    print('saving obj for',path)
     folder = os.path.dirname(path)
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -174,6 +174,7 @@ def getTableFile(path,short):
 #gets calendar department links, loads file if exists, tests loaded obj, removes erroneous file and resets on failure
 def parse_depts(departments_path):
     global departments_calendar_load_fail
+    global name_to_short
     loaded,faculties = load_or_scrape(ucalgary_root,departments_path)
     if loaded:
         departments_calendar_load_fail = False
@@ -195,6 +196,7 @@ def parse_depts(departments_path):
                         title,short = ' '.join([s for s in title_short]),''
                     if short and title and len(short) > 1:
                         departments[short]={'title':title,'link':link,'class':{}}
+                        name_to_short[title] = short
         if type(departments) == type({}) and 'ARKY' in departments.keys():
             save_objs(departments_path, departments)
         else:
@@ -234,17 +236,18 @@ def load_or_scrape(root,path):
     if os.path.exists(path):
         with open(path) as f:
             html_request = False
-            print('using local obj file')
-            input()
             return True, jsonpickle.decode(json.load(f))
     else:
         if os.path.exists(raw+path):
             #can get html locally
             with open(raw+path) as f:
-                print('using local html',raw+path)
-                input()
-                print('returning now')
-                return False, f.read()
+                content = f.read()
+                content = content.replace('\\xc3\\xa7','ç').replace('&amp;','&').replace('\\xc2\\xa0',' ').replace('\\xc3\\xa9','é')
+                content = content.replace('\\xc3\\xbc','ü').replace('&#039;',"'").replace('\\xc3\\xa1','á')
+                content = content.replace('\\xc3\\xad','í').replace('\\xc3\\xba','ú').replace('\\xc3\\xb6','ö')
+                content = content.replace('\\xc2\\xa0',' ').replace('&#x0A','\n').replace('&nbsp;',' ').replace('\t',' ')
+                return False, content
+
         html_request = True
         
         folder = os.path.dirname(raw+path)
@@ -253,8 +256,6 @@ def load_or_scrape(root,path):
             os.makedirs(folder)
         
         lines = requests.get(root+path,headers=headers).iter_lines()
-        print('usng remote html')
-        input()
         with open(raw+path,'w') as f:
             for line in lines:
                 f.write(str(line))
@@ -265,12 +266,11 @@ def load_or_scrape(root,path):
 def parse_faculties():
     global repeat
     global contacts_departments_load_fail
+
     #if exist, open, else scrape
     loaded,faculties = load_or_scrape(contacts_root,faculties_path)
     if loaded:
-        print('returning faculties obj')
         return faculties
-    print('building new faculties obj')
 #    else we scraped, we have parsing to do.
     html = ''.join([str(s) for s in faculties])
 
@@ -332,13 +332,13 @@ def parse_faculties():
 
         faculties[faculty] = divided
         #Arts Exists
-    if type(faculties) == type({}) and 'arts' in faculties:#expand to a thorough test
+    if type(faculties) == type({}) and len(faculties.keys()) > 0:#expand to a thorough test
         save_objs(faculties_path,faculties)
     else:
         if repeat == 3:
             return None
         repeat = repeat + 1
-        os.remove(faculties+path)
+        os.remove(faculties_path)
         contacts_departments_load_fail = True
         faculties = parse_faclties()
     contacts_classes_load_fail = False
@@ -350,12 +350,51 @@ def parse_faculties():
 
     
 def parse_schedules(short):
+    #should load the object associated with 'tag'
+    # except when that tag is a top level tag for other embedded classes, such as SENG under CPSC
+
+    # if the short is a top level tag,ie, CPSC for SENG, it will:
+    #   load only cpsc if cpsc exists
+    #   parse cpsc, find cpsc, data, isec, and seng and will save each
+    #
+    # if the short is not top level, such as SENG under CPSC, it will:
+    #   load only if seng already exists
+    #   fail to load if seng does not exist, and will fail to find it under seng if it tries
+    #
+    # may need to rethink the loading process.
+    # I will have all the infoin one go, but there will be some failures that are not actually falures along the way, guaranteed
     
 #    if exist, open, else scrape
-    try:
-        os.remove(class_day_time_path % short)
-    except:
-        pass
+
+    #special cases - these allow finding 49 more fields which aren't directly referenced
+    #52 remain unknowns
+    specials = {
+        'acct':'ha',
+        'almc':'slllc',
+        'astr':'phas',
+        'bcem':'bio',
+        'bmen':'biomedical',
+        'iphe':'sw',
+        'knes':'kn',
+        'neur':'bscneuro',
+        'scie':'natsci',
+        'glgy':'geos'
+        }
+    if short in specials:
+        short = specials[short]
+#    if short == 'knes':
+#        short = 'kn'
+#    elif short == 'acct':
+#        short = 'ha'
+#    elif short == 'almc':
+#        short = 'slllc'
+#    elif short == 'bcem':
+#        short='bio'
+#    elif short == 'scie':
+#        short='natsci'
+#    elif short == 'glgy':
+#        short = 'geos'
+    
     loaded,classes = load_or_scrape(contacts_root,class_day_time_path % short)
     if loaded:
         return classes
@@ -368,25 +407,39 @@ def parse_schedules(short):
         return
     html = ''.join(html)
     departments = html.split('unitis-courses-heading')[1:]
+    if len(departments) == 0:
+        return None
     department = {}
     for dept in departments:
         dept = dept[1:] #trim the tag we broke
-        dept_short = re.search('[A-Z]{3,4}',re.search('[A-Z]{3,4} [0-9]{3}',dept).group()).group()
+        try:
+            dept_short = (re.search('[A-Z]{2,5}',re.search('>[A-Z]{2,5} [0-9]{3}( |\.|[A-Z].)',dept).group()).group())
+        except:
+            print(dept)
+            input()
         department[dept_short] = slice_course_table(dept_short,dept)
+        
+    if len(department) > 0:
+        save_objs(class_day_time_path % short,department)
     return department
 
 def slice_course_table(short,html):
     global repeat
     global contacts_classes_load_fail
-    indices = [i.span()[0] for i in re.finditer('[A-Z]{3,4} [0-9]{3}',html)]
+    indices = [i.span()[0]+1 for i in re.finditer('>[A-Z]{2,5} [0-9]{3}( |\.)',html)]
     indices.insert(0,0)
     classes = {}
+    p = True
+        
     for info in [html[indices[i]:indices[i+1]] for i in range(len(indices)-1)][1:]:
+        info_old = info
+#        info = strip_tags(info)
         short,info = info.split(' ',1)[0],info.split(' ',1)[1]
         num,info = info.split(' ',1)[0],(info.split(' ',1)[1])[2:]
         name,info = info[:info.find('<')],info[info.find('<'):]
         info = info.replace('>SEM','>SEM_SEM').replace('>LEC ','>LEC_LEC ').replace('>TUT ','>TUT_TUT ').replace('>LAB ','>LAB_LAB ').replace('>Notes:','>NOTES_Notes:').replace('>Details<','>DETAILS_Details<')
-        info = info.replace('\t',' ').replace('\\xc2\\xa0',' ').replace('&nbsp;',' ').replace('>TBA','> TBA').replace('&#x0A','')
+        info = info.replace('>TBA','> TBA')
+        
         details = re.split('SEM_|LEC_|TUT_|LAB_|NOTES_|DETAILS_|<br />',info)
         CLS=[]
         RMS=[]
@@ -394,8 +447,18 @@ def slice_course_table(short,html):
         NTS=[]
         DET=[]
         this = {}
+#        if '>' in name or '<' in name or '>' in num or '<' in num or '>' in short or '<' in short:
+#            print(info_old)
+#            print('====')
+#            print("'"+short+"'"+"'"+num+"'"+"'"+name+"'")
+#            input()
+
         for d in details:
             s = strip_tags(d)
+#            if '>' in s or '<' in s:
+#                print('s:  ',s)
+#                print('d:  ',d)
+#                input()
             match=re.search('[0-9]{2}:[0-9]{2} - [0-9]{2}:[0-9]{2}',s)
             if match or 'TBA' in s:
                 s = s.split(' ',2)
@@ -412,34 +475,21 @@ def slice_course_table(short,html):
             
         c = zip(CLS,RMS,PRF)
         for x in c:
-            this['type'] = x[0][0]
             p_num = re.match('[0-9]{1,2}',x[0][1])
             day = re.search('([A-Z]+)',x[0][1])
             if p_num is not None:
-                this['p_num'] = p_num.group(0)
+                p_num = int(p_num.group(0))
             if day is not None:
-                this['day']  = day.group(0)
-            this['time'] = x[0][2]
-            this['room'] = x[1]
-            this['prof'] = x[2]
+                day  = day.group(0)
+            t = x[0][0]
+            if t not in this:
+                this[t] = {}
+            this[t][p_num] = {}
+            this[t][p_num]['day'] = day
+            this[t][p_num]['time'] = x[0][2]
+            this[t][p_num]['room'] = x[1]
+            this[t][p_num]['prof'] = x[2]
         classes[num]={'num':num,'name':name,'short':short,'details':this}
-    if type(classes) == type({}) and len(classes.keys()) > 0:
-        print('saving object for',class_day_time_path % short)
-        save_objs(class_day_time_path % short,classes)
-        contacts_classes_load_fail = False
-    else:
-        print('not saving object for',class_day_time_path % short)
-        try:
-            print('\t',type(classes),len(classes.keys()),classes.keys())
-        except:
-            print('\t',type(classes))
-        if repeat == 3:
-            return None
-        repeat = repeat + 1
-        if os.path.exists(class_day_time_path % short):
-            os.remove(class_day_time_path % short)
-        contacts_classes_load_fail = True
-        classes = parse_schedules(short)
     return classes
 
         
@@ -448,39 +498,59 @@ def scrape_department_and_class_reqs():
     departments = parse_depts(departments_path)
     full_class_info = {}
     finished_departments = False
-    delayed=False
     while not finished_departments:
         for d in departments.keys():
             repeat = 0   #reset the repeat trigger
             full_class_info[d] = DEPT(d,departments[d]['title'],getTableFile(departments[d]['link'],d))
-            if repeat == 3:  #probably got myself banned, parsing errors are likely to crash out the script
-                if delayed:
-                    increase_delays()
-                    delayed=False
-                delay('reject')  #sleep 5 minutes to try and get unbanned
-                delayed=True
-                break       #exit current loop, to startthe process over on iterate while
-            #if either fails, not_finished is still true, we need to cycle around again.
-            if html_request:
-                delay(random.randint(1,5))
         finished_departments = not(departments_calendar_load_fail or departments_calendar_class_tables_load_fail)
     return full_class_info
 
 def scrape_faculties_and_class_schedules(depts):
     faculties = parse_faculties()
-    for short in faculties.keys():
-        schedules = parse_schedules(short.lower())
+    departments = {}
+    for dept in depts.keys():
+#        print(dept)
+        schedules = parse_schedules(dept.lower())
+        if schedules is not None:
+            for short in schedules.keys():
+                    
+                departments[short] = schedules[short]
+    return departments
 
 def get_everything():
     print('getting class requisites')
     departments = scrape_department_and_class_reqs()
     print('getting class schedules')
-    scrape_faculties_and_class_schedules(departments)
+    faculties = scrape_faculties_and_class_schedules(departments)
     print('data scrape/load completed')
-    for dept in departments.keys():
-#        print(dept)
-        print('\t',departments[dept].keys())
+    not_found = [x for x in departments.keys() if x not in faculties.keys()]
+    found = {}
+    for key in departments.keys():
+        if key in faculties:
+            found[key] = departments[key]
+            found[key].IDS = faculties[key]
+    dept_count = 0
+    class_count = 0
+    schedule_count = 0
 
-#get_everything()
-
-print(parse_schedules('cpsc').keys())
+    last_dept=None
+    last_class=None
+    
+    for key in sorted(found.keys()):
+        for i in sorted(found[key].IDS.keys()):
+            if len(found[key].IDS[i]['details'].keys()) > 0:
+#                print(found[key].IDS[i]['short'],found[key].IDS[i]['num'],found[key].IDS[i]['name'])
+                for t in ['SEM','LEC','LAB','TUT']:
+                    if t in found[key].IDS[i]['details']:
+                        for n in sorted(found[key].IDS[i]['details'][t].keys()):
+#                            print('\t',t,n,found[key].IDS[i]['details'][t][n]['day'],found[key].IDS[i]['details'][t][n]['time'],found[key].IDS[i]['details'][t][n]['room'],found[key].IDS[i]['details'][t][n]['prof'])
+                            schedule_count = schedule_count+1
+                            if key != last_dept:
+                                dept_count = dept_count+1
+                                last_dept = key
+                            if found[key].IDS[i] != last_class:
+                                class_count = class_count +1
+                                last_class = found[key].IDS[i]
+#    print('contains information on',schedule_count,'separate schedule items')
+    print(dept_count,'departments,',class_count,'classes,',schedule_count,'schedule items')
+get_everything()
